c2b9c4acc128966ff8d5979a593f96581f90a7e5
Oscar Moreno <oscarpol95@gmail.com>
2025-09-12 12:41:55 -0500

    feat: update main

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+from typing import List, Union
+
 from dotenv import load_dotenv
 
-from langchain_ollama import ChatOllama
+# from langchain_ollama import ChatOllama
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 from langchain.agents.format_scratchpad import format_log_to_str
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.prompts import PromptTemplate
-from langchain_core.tools import tool, render_text_description
+from langchain_core.tools import Tool, tool, render_text_description
 from langchain_openai import ChatOpenAI
+
+
+from callbacks import AgentCallbackHandler
 
 load_dotenv()
 
@@ -15,7 +22,18 @@ load_dotenv()
 @tool
 def get_text_length(text: str) -> int:
     """Returns the length of a text by characters"""
+    print(f"get_text_length enter with {text=}")
+    text = text.strip("'\n").strip(
+        '"'
+    )  # stripping away non alphabetic characters just in case
     return len(text)
+
+
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool wtih name {tool_name} not found")
 
 
 def main():
@@ -49,14 +67,10 @@ def main():
         tool_names=", ".join([t.name for t in tools]),
     )
 
-    llm = ChatOllama(
-        temperature=0, model="llama3.1:8b", stop=["\nObservation", "Observation"]
+    llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", callbacks=[AgentCallbackHandler()]).bind(
+        stop=["\nObservation:", "Observation:"]
     )
-
-    # llm = ChatOpenAI(
-    #    temperature=0,
-    #    stop=["\nObservation", "Observation"],
-    # )
+    intermediate_steps = []
 
     agent = (
         {
@@ -68,12 +82,33 @@ def main():
         | ReActSingleInputOutputParser()
     )
 
-    res = agent.invoke(
-        {"input": "What is the length of the word: 'DOG' in characters ? "}
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {
+            "input": "What is the length of the word: DOG ?",
+            "agent_scratchpad": intermediate_steps,
+        }
     )
+    print(agent_step)
 
-    print(res)
+    if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tools, tool_name)
+        tool_input = agent_step.tool_input
 
+        observation = tool_to_use.func(str(tool_input))
+        print(f"{observation=}")
+        intermediate_steps.append((agent_step, str(observation)))
+
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {
+            "input": "What is the length of the word: DOG?",
+            "agent_scratchpad": intermediate_steps,
+        }
+    )
+    print("second:", agent_step)
+    if isinstance(agent_step, AgentFinish):
+        print("### AgentFinish ###")
+        print(agent_step.return_values)
 
 if __name__ == "__main__":
     main()
