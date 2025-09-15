b6df316dde580ed0e94ef71e17347d01c9947e71
Oscar Moreno <oscarpol95@gmail.com>
2025-09-15 16:59:00 -0500

    feat: medium analyzer ingestion implementation

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 
 import os
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
 
 def main():
-    print("Hello from langchain-course!")
+    print("Ingesting ...")
+    loader = TextLoader("mediumblog1.txt", encoding="utf-8")
+    document = loader.load()
+    print(f"Document length: {len(document)}")
 
+    print("splitting ... ")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"Created {len(texts)} chunks")
+
+    embeddings = OpenAIEmbeddings(
+        openai_api_key=os.environ.get("OPENAI_API_KEY"))
+
+    print("Ingesting ...")
+    PineconeVectorStore.from_documents(
+        texts,
+        embedding=embeddings,
+        index_name=os.environ['INDEX_NAME']
+    )
+    print("Finish ...")
 
 if __name__ == "__main__":
     main()
